3fa12c9f3170cb627927d9d447d6d6256ac4faf0
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-07-31 17:59:23 +0300

    Update JavaScriptBasics.js

--- JavaScriptBasics.js
+++ JavaScriptBasics.js
@@ -1,7 +1,13 @@
-var name = 'value' // str/int/bool/null variable
+// JS uses camelCase
+
+var name = "value"; // str/int/bool/null variable
 
 var array1 = [] // can be modified
 const array2 = [] // cannot be modified
+array1.push() // add to array
+array1.splice(index, n) // index to start removing; n = num of items to be removed
+
+const index = array1.indexOf(name) // get index of passed parameter
 
 var condition = null 
 
@@ -12,6 +18,11 @@ if (condition) {
     pass;
 } else {
     pass;
+}
+
+// While loops
+while (true){
+    continue;
 }
 
 // For loops
@@ -29,5 +40,20 @@ function newFunction(param1, param2) {
     return;
 }
 
+// Objects
+var objectName = {key:'value'}; // the key is not a string when initializing
+objectName['key'] // but when getting value at key it is passed as a string
+objectName['key'] = 'new_value' // update value
 
+// iterating through objects
+for (key in carInfo){
+    console.log(key);
+    console.log(carInfo[key]);
+}
 
+var newObject = {
+    name: 'Asd',
+    greet: function(){
+        console.log('Hello ' + this.name)
+    }
+}
