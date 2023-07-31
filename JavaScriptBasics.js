edd0fa202c2b2348813b0cf6805a051f01ca5f13
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-07-31 17:02:03 +0300

    Add files via upload

---
+++ JavaScriptBasics.js
@@ -0,0 +1,33 @@
+var name = 'value' // str/int/bool/null variable
+
+var array1 = [] // can be modified
+const array2 = [] // cannot be modified
+
+var condition = null 
+
+// Control Flow
+if (condition) {
+    pass;
+} else if (!condition){
+    pass;
+} else {
+    pass;
+}
+
+// For loops
+for (var i=0; i<10; i++){
+    continue;
+}
+
+for (var num of array){
+    continue;
+}
+array.forEach(newFunction); // for each item in the array call function
+
+// Functions
+function newFunction(param1, param2) {
+    return;
+}
+
+
+
