cc422a9973b19cf173399531d5507af2ffe1f975
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-08-08 15:01:28 +0300

    remove venv

---
+++ django/django.py
@@ -0,0 +1,26 @@
+'''
+SET UP A NEW VIRTUAL ENVIRONMENT
+python -m venv django_venv 
+
+ACTIVATE VENV
+django_venv\scripts\activate.bat
+
+DEACTIVATE VENV
+django_venv\scripts\deactivate.bat
+
+
+CREATE A NEW PROJECT
+After installing Django, use 'django-admin startproject name'
+to create a project template
+
+START SERVER
+Go to the project folder and run 'python manage.py runserver'
+
+DJANGO APPLICATIONS - 
+different apps that can be integrated into multiple projects that serve some function
+
+To create a new app - run 'python manage.py startapp first_app'
+
+If you want to add it into your website, you need to add the new app to the 
+settings.py of your project
+'''
