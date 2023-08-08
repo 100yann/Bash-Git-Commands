36331b67c312e8e78ea53684695582df924cd351
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-08-08 15:21:48 +0300

    use include() to add a url map

--- django/first_project/first_project/urls.py
+++ django/first_project/first_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from first_app import views
+from django.conf.urls import include
 
 urlpatterns = [
-    re_path(r'.*',views.index, name='index'),
+    path('first_app/', include('first_app.urls')),
     path('admin/', admin.site.urls),
 ]
